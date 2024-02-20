7285678c126f46a4c72bad8289038df5b9303dac
parteekmalik <parteekmalik5@gmail.com>
2024-02-20 17:36:53 +0530

    removed trpc react code

--- src/app/zerodha/_components/Home.tsx
+++ src/app/zerodha/_components/Home.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { useContext, useEffect } from "react";
-import DataContext from "../_contexts/data/data";
+import { useContext } from "react";
 import type { TuserDetails } from "../_contexts/data/data";
+import DataContext from "../_contexts/data/data";
 import OrderForm from "./OrderForm/orderForm";
 import RightSide from "./Rightside/RightSde";
 import WatchList from "./WatchList/watchList";
 import Debugdata from "./debugdata";
 import Header from "./hearder";
-import { api } from "~/trpc/react";
 
 interface IHome {
   watchlist: string[][];
@@ -16,19 +15,19 @@ interface IHome {
 export default function Home() {
   const { dataDispatch } = useContext(DataContext);
 
-  const watchlist = api.accountInfo.watchList.useQuery().data ?? [];
-  const userInfo = api.accountInfo.info.useQuery().data ?? {
-    name: "not_found",
-    email: "not_found@gmail.com",
-    image: "not_found",
-    id: "not_found",
-  };
-  useEffect(() => {
-    if (userInfo.id !== "not_found")
-      dataDispatch({ type: "update_userDetails", payload: userInfo });
-    if (watchlist.length !== 0)
-      dataDispatch({ type: "update_watchList", payload: watchlist });
-  }, [watchlist, userInfo]);
+  // const watchlist = api.accountInfo.watchList.useQuery().data ?? [];
+  // const userInfo = api.accountInfo.info.useQuery().data ?? {
+  //   name: "not_found",
+  //   email: "not_found@gmail.com",
+  //   image: "not_found",
+  //   id: "not_found",
+  // };
+  // useEffect(() => {
+  //   if (userInfo.id !== "not_found")
+  //     dataDispatch({ type: "update_userDetails", payload: userInfo });
+  //   if (watchlist.length !== 0)
+  //     dataDispatch({ type: "update_watchList", payload: watchlist });
+  // }, [watchlist, userInfo]);
 
   return (
     <main className=" max-w-screen flex h-screen max-h-screen w-screen flex-col  items-center justify-center bg-[#f9f9f9] font-['Open_Sans','sans-serif']  ">
