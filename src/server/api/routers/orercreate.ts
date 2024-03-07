b97cb93935644c54928b3fdcdf8f5859712b500b
parteekmalik <parteekmalik5@gmail.com>
2024-03-07 13:08:09 +0530

    debug console.log

--- src/server/api/routers/orercreate.ts
+++ src/server/api/routers/orercreate.ts
@@ -70,11 +70,13 @@ export const orderRouter = createTRPCRouter({
           });
         console.log("order transection comleted");
       }
+      return "sucess";
     }),
 });
 async function getLTP(symbol: string) {
   const url = "https://api.binance.com/api/v3/ticker/price?symbol=" + symbol;
   console.log("url", url);
+  console.log("axios", await axios.get(url));
   const res = (await axios.get(url)).data as { symbol: string; price: string };
   console.log("getLTP " + symbol + " -> ", res);
   return Number(res.price);
