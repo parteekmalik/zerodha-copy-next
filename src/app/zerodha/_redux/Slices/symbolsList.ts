2076f591e78e864d4ceea62a0015933da1c3e1a1
parteekmalik <parteekmalik5@gmail.com>
2024-04-13 17:43:12 +0530

    changed type

---
+++ src/app/zerodha/_redux/Slices/symbolsList.ts
@@ -0,0 +1,41 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { JSONType } from "public/symbolNameType";
+import { listToRecord } from "../../utils";
+type TtickerChangeType = {
+  quoteAsset: string;
+  status: string;
+  symbol: string;
+  baseAsset: string;
+  name: string;
+};
+
+export type TsymbolsListType = Record<string, TtickerChangeType>;
+
+const initialState: TsymbolsListType = {};
+const symbolsListSlice = createSlice({
+  name: "symbolsListType",
+  initialState,
+  reducers: {
+    updateSymbolsList: (state, action: PayloadAction<JSONType[]>) => {
+      console.log(action);
+      const symbolsList = listToRecord(
+        action.payload
+          .map((item) => {
+            return {
+              quoteAsset: item.quoteAsset,
+              status: item.status,
+              symbol: item.symbol,
+              baseAsset: item.baseAsset,
+              name: item.name,
+            };
+          })
+          .filter((i) => i.status === "TRADING"),
+        "symbol",
+      );
+      return symbolsList;
+    },
+  },
+});
+export const { updateSymbolsList } = symbolsListSlice.actions;
+
+export default symbolsListSlice.reducer;
