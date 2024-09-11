c5a6cf875f9d1a647e91c71609dfd1728a75c94d
parteekmalik <parteekmalik5@gmail.com>
2024-09-11 16:43:05 +0530

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,37 @@
-export { default } from 'next-auth/middleware';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
 
-//export const config = {
-//  matcher: [  ],
-//};
+export function middleware(request: NextRequest) {
+  console.log(request);
+  const path = request.nextUrl.pathname
+
+  const isPublicPath = path === '/api/auth/signin' || path === '/signup' || path === '/verifyemail'
+
+  const token = request.cookies.get('token')?.value ?? ''
+
+  if(isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+  }
+
+  if (!isPublicPath && !token) {
+    // If user is not authenticated, store the requested page and redirect to login
+    const redirectUrl = new URL('/api/auth/signin', request.nextUrl);
+    redirectUrl.searchParams.set('next', path); // Store the originally requested path
+    return NextResponse.redirect(redirectUrl);
+}
+    
+}
+
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/:path*',
+    '/',
+    '/profile',
+    '/login',
+    '/signup',
+    '/verifyemail'
+  ]
+}
