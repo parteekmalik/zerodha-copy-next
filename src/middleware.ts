c5108d66688576a99611c86ede9e4f7964917dd6
parteekmalik <parteekmalik5@gmail.com>
2024-09-12 15:25:32 +0530

    fix: login

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,25 +7,22 @@ export function middleware(request: NextRequest) {
   const publicPaths = ["/api/auth/signin","/api/auth/signin/discord","/api/auth/callback/discord"];
   const isPublicPath = publicPaths.includes(path);
 
-  const token = request.cookies.get("next-auth.session-token")?.value;
-
+  const token = request.cookies.get("next-auth.session-token")?.value ?? request.cookies.get("__Secure-next-auth.session-token")?.value;
+  
   console.log(path, isPublicPath, token, request.cookies);
 
   // Redirect from root ("/") to "/Dashboard"
   if (path === "/kite" || path === "/") {
     return NextResponse.redirect(new URL("/kite/Dashboard", request.nextUrl));
   }
-
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL("/", request.nextUrl));
-  }
-
+  
   if (!isPublicPath && !token) {
     // If user is not authenticated, store the requested page and redirect to login
     const redirectUrl = new URL("/api/auth/signin", request.nextUrl);
     redirectUrl.searchParams.set("next", path); // Store the originally requested path
     return NextResponse.redirect(redirectUrl);
   }
+  return NextResponse.next();
 }
 
 // Apply middleware to all paths except those starting with "/_next"
