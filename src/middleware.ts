a78b60c530ad8e7dc1a1f6a2c507b7f88f471e3e
parteekmalik <parteekmalik5@gmail.com>
2024-09-29 14:41:14 +0530

    fix: login not working google

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,24 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
-  const publicPaths = ["/api/auth/signin","/api/auth/signin/discord","/api/auth/callback/discord"];
+  const publicPaths = [
+    "/api/auth/signin",
+    "/api/auth/signin/discord",
+    "/api/auth/callback/discord",
+    "/api/auth/signin/google",
+    "/api/auth/callback/google",
+  ];
   const isPublicPath = publicPaths.includes(path);
 
-  const token =  request.cookies.get("__Secure-next-auth.session-token")?.value ?? request.cookies.get("next-auth.session-token")?.value ;
+  const token =
+    request.cookies.get("__Secure-next-auth.session-token")?.value ??
+    request.cookies.get("next-auth.session-token")?.value;
 
   // Redirect from root ("/") to "/Dashboard"
   if (path === "/kite" || path === "/") {
     return NextResponse.redirect(new URL("/kite/Dashboard", request.nextUrl));
   }
-  
+
   if (!isPublicPath && !token) {
     // If user is not authenticated, store the requested page and redirect to login
     const redirectUrl = new URL("/api/auth/signin", request.nextUrl);
@@ -25,5 +33,5 @@ export function middleware(request: NextRequest) {
 
 // Apply middleware to all paths except those starting with "/_next"
 export const config = {
-  matcher: ["/","/kite","/kite/(.*)", "/api/(.*)"],
+  matcher: ["/", "/kite", "/kite/(.*)", "/api/(.*)"],
 };
